1623a7eef015cd9837bb4f1b8fdc710ff8c5bc23
tony056 <tony61507@gmail.com>
2015-09-24 21:40:42 +0800

    layout and init configuration

--- SimpleUI/app/src/main/java/com/example/simpleui/LoginActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/LoginActivity.java
@@ -1,10 +1,19 @@
 package com.example.simpleui;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.FacebookSdk;
 
 /**
  * Created by tungying-chao on 9/24/15.
  */
 public class LoginActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
