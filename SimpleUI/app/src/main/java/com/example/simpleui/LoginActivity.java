2ff2ee6d158a5da090f8f0ef47f48b109d06844d
tony056 <tony61507@gmail.com>
2015-09-24 22:07:32 +0800

    Added onActivityResult to invoke fb callback

--- SimpleUI/app/src/main/java/com/example/simpleui/LoginActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/LoginActivity.java
@@ -52,4 +52,9 @@ public class LoginActivity extends AppCompatActivity {
         intent.putExtra("userId", userId);
         startActivity(intent);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
