aec4d70d5246458a98f0bf4c45fe7c1d39e6f03b
tony056 <tony61507@gmail.com>
2015-09-24 21:26:35 +0800

    Config fb login setup

---
+++ SimpleUI/app/src/main/java/com/example/simpleui/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.simpleui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tungying-chao on 9/24/15.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+}
--- SimpleUI/app/src/main/java/com/example/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/OrderDetailActivity.java
@@ -23,7 +23,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-
 public class OrderDetailActivity extends AppCompatActivity
         implements OnMapReadyCallback {
 
